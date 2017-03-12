d2d845ed27128224eeb369f8f27fa0fb5375259a
Gijsjan <agijsbro@gmail.com>
2017-03-12 08:50:21 +0100

    Remove unused deps

--- build/index.js
+++ build/index.js
@@ -1,8 +1,15 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const React = require("react");
-const option_1 = require("./option");
-const sort_1 = require("./sort");
+var __assign = (this && this.__assign) || Object.assign || function(t) {
+    for (var s, i = 1, n = arguments.length; i < n; i++) {
+        s = arguments[i];
+        for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+            t[p] = s[p];
+    }
+    return t;
+};
+const React = require('react');
+const option_1 = require('./option');
+const sort_1 = require('./sort');
 class Options extends React.Component {
     constructor() {
         super(...arguments);
@@ -44,16 +51,16 @@ class Options extends React.Component {
         if (this.state.values.length === 0 && this.props.children == null) {
             return null;
         }
-        const listitems = this.state.values.map((optionData, index) => React.createElement(option_1.default, Object.assign({}, this.props, { active: this.state.activeIndex === index, key: index, optionData: optionData, onClick: () => this.setState({ activeIndex: index }, () => this.select()) })));
+        const listitems = this.state.values.map((optionData, index) => React.createElement(option_1.default, __assign({}, this.props, {active: this.state.activeIndex === index, key: index, optionData: optionData, onClick: () => this.setState({ activeIndex: index }, () => this.select())})));
         const children = this.props.children != null ?
-            React.createElement("li", { className: "children" }, this.props.children) :
+            React.createElement("li", {className: "children"}, this.props.children) :
             null;
-        return (React.createElement("ul", { className: "hire-options", ref: (node) => {
-                if (node != null) {
-                    node.style.zIndex = '1000';
-                }
-            } },
-            children,
+        return (React.createElement("ul", {className: "hire-options", ref: (node) => {
+            if (node != null) {
+                node.style.zIndex = '1000';
+            }
+        }}, 
+            children, 
             listitems));
     }
 }
@@ -64,4 +71,5 @@ Options.defaultProps = {
     value: { key: '', value: '' },
     values: [],
 };
+Object.defineProperty(exports, "__esModule", { value: true });
 exports.default = Options;
--- build/sort.js
+++ build/sort.js
@@ -1,5 +1,4 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
 const sortOnRelevance = (values, query) => values.sort((a, b) => {
     a = a.value.toLowerCase();
     b = b.value.toLowerCase();
